847e88a0ebfaa01004e724b0c673153142d120ea
Gaia <gmejiasg@purdue.edu>
2025-04-23 18:21:56 -0400

    Adding placeholder assets & some components

---
+++ src/pages/ProjectDetailPage.jsx
@@ -0,0 +1,20 @@
+import ProjectAbout from '../components/ProjectAbout';
+import Header from '../components/Header';
+import Wrapper from '../components/Wrapper';
+
+const ProjectDetail = () => {
+    return (
+        <>
+
+            <Header />
+
+            <Wrapper>
+                <ProjectAbout />
+            </Wrapper>
+
+        </>
+
+    );
+};
+
+export default ProjectDetail;
