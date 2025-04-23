a470be79f7ba4613654e9c247dfb8abe0d0a496a
Annabelle Bridgewaters <apbridge@purdue.edu>
2025-04-23 00:05:19 -0400

    add content

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,5 @@
+import { Link } from "react-router-dom";
+const Navbar = () => {
+
+};
+export default Navbar;
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,8 @@
+const AboutPage = () => {
+    return (
+        <Wrapper>
+
+        </Wrapper>
+    );
+};
+export default AboutPage;
